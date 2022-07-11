f4655a46c5e850e79ad4ac865faa95abd5e85059
Yao Wang <97422448+lvSojen@users.noreply.github.com>
2022-07-10 21:21:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-try {
-  const music = new Audio("./music/Github Skyline.mp3");
-}
-catch(err) {
-  cosnt music = new Audio("../music/Github Skyline.mp3");
-}
+const music = new Audio("../music/Github Skyline.mp3");
 music.play();
 music.loop = true;
