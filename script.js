8079aec2805792624b7081b7e41e15447d60150b
Yao Wang <97422448+lvSojen@users.noreply.github.com>
2022-07-10 21:15:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const music = new Audio("./music/Github Skyline.mp3");
-if(isNaN(sound.duration)) {
-  music = new Audio("../music/Github Skyline.mp3");
+
+try {
+  const music = new Audio("./music/Github Skyline.mp3");
+}
+catch(err) {
+  cosnt music = new Audio("../music/Github Skyline.mp3");
 }
 music.play();
 music.loop = true;
